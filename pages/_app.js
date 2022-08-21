4830ab4e58b125d0007d362017a3957623fcb2ce
Caio Bueno <caiobueno@Caios-MacBook-Pro.local>
2022-08-20 22:22:10 -0400

    done

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
+import { FormProvider } from 'upscale-forms'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <FormProvider
+      preLoad={[28, 29]}
+    >
       <Component {...pageProps} />
+    </FormProvider>
   )
 }
 
